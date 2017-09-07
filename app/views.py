63d8c2d00e59107bc2475fdfbfc302d85f5c2d0a
Troels Ynddal <troelsynddal@gmail.com>
2017-09-07 15:19:38 +0200

    Added alive route

--- app/views.py
+++ app/views.py
@@ -21,14 +21,27 @@ def getCalendar(user):
     handle = urllib2.urlopen('https://personligtskema.ku.dk/ical.asp?objectclass=student&id=%s' % user)
     data = handle.read(1024**2)  # Max 1MB
 
-    if len(data) < 20*1024:  # 20 KB (Tested data was >75 KB for success and <9 KB for errors)
+    if len(data) < 10*1024:  # 10 KB (Tested data was >75 KB for success and <9 KB for errors)
         print "Data from server too small - probably an error. A cache will be used instead."
         cached = cacheMap.get(user)
         return "No cache available" if cached is None else cached
     else:
-        #print "Request successfull"
+        #print "Request successful"
         cacheMap[user] = data
         return data
+
+
+@app.route('/alive', methods=['GET'])
+def alive():
+    nocache = False
+    for k, v in cacheMap.iteritems():
+        if v is None:
+            nocache = True
+
+    if nocache:
+        return "NO CACHE"
+    else:
+        return "OK"
 
 
 @app.route('/getiCal', methods=['GET'])
